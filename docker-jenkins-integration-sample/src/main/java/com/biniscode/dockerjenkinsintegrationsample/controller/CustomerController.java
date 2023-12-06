3d5d994363c66df1fa264305beb8930629d4737b
biniam.solomon <biniam.solomon@safaricom.et>
2023-12-06 12:04:16 +0300

    added

--- docker-jenkins-integration-sample/src/main/java/com/biniscode/dockerjenkinsintegrationsample/controller/CustomerController.java
+++ docker-jenkins-integration-sample/src/main/java/com/biniscode/dockerjenkinsintegrationsample/controller/CustomerController.java
@@ -1,6 +1,8 @@
 package com.biniscode.dockerjenkinsintegrationsample.controller;
 
 import com.biniscode.dockerjenkinsintegrationsample.entity.Customer;
+import com.biniscode.dockerjenkinsintegrationsample.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
+    @Autowired
+    CustomerService customerService;
 
     @GetMapping("/a1")
     public Customer customer(){
-        return new Customer("1234", "Bekele");
+        return customerService.register();
     }
 }
--- docker-jenkins-integration-sample/src/main/java/com/biniscode/dockerjenkinsintegrationsample/service/CustomerService.java
+++ docker-jenkins-integration-sample/src/main/java/com/biniscode/dockerjenkinsintegrationsample/service/CustomerService.java
@@ -1,2 +1,11 @@
-package com.biniscode.dockerjenkinsintegrationsample.service;public class CustomerController {
+package com.biniscode.dockerjenkinsintegrationsample.service;
+
+import com.biniscode.dockerjenkinsintegrationsample.entity.Customer;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+    public Customer register() {
+        return new Customer("1234","bin");
+    }
 }
