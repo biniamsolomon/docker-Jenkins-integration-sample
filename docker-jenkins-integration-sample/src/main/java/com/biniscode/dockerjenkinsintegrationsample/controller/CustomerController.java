4e709202a34b3ace0089bb97317bbc59e9bebcdc
biniam.solomon <biniam.solomon@safaricom.et>
2023-12-06 11:56:06 +0300

    added

--- docker-jenkins-integration-sample/src/main/java/com/biniscode/dockerjenkinsintegrationsample/controller/CustomerController.java
+++ docker-jenkins-integration-sample/src/main/java/com/biniscode/dockerjenkinsintegrationsample/controller/CustomerController.java
@@ -1,2 +1,17 @@
-package com.biniscode.dockerjenkinsintegrationsample.controller;public class CustomerController {
+package com.biniscode.dockerjenkinsintegrationsample.controller;
+
+import com.biniscode.dockerjenkinsintegrationsample.entity.Customer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    @GetMapping("/a1")
+    public Customer customer(){
+        return new Customer("1234", "Bekele");
+    }
 }
